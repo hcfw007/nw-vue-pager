2b049054f1ea35a8330f77d0a3dd229e6ca81e52
NickWang <hcfw007@126.com>
2018-05-11 16:07:03 +0800

    initial setup

---
+++ src/Pager-Content.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  name: 'Pager-Content',
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import content from './Pager-Content.vue'
+import pagination from './Pager-Pagination.vue'
+
+const pagerPlugin = {
+  install (Vue, options = {}) {
+
+  }
+}
+
+module.exports = pagerPlugin
